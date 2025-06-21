2a89882e9d7576ce5ef67349f80a29fd6e05d9ad
maheeen-khan <maheeenkhan4@gmail.com>
2025-06-21 19:46:18 +0500

    updated

--- frontend/src/pages/OrdersPage.jsx
+++ frontend/src/pages/OrdersPage.jsx
@@ -7,15 +7,15 @@ const OrdersPage = () => {
     <>
       <Box sx={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center' }}>
 
-        <Box sx={{ alignItems: 'center', backgroundColor: '#6F4E37', color: 'white', padding: '10px', paddingX: '22px', borderRadius: '7px', width: '24%' }}>Total Orders <h1>71</h1> <p style={{ fontSize: '13px' }}>22% last week</p></Box>
+        <Box sx={{ alignItems: 'center', backgroundColor: '#453F78', color: 'white', padding: '10px', paddingX: '22px', borderRadius: '7px', width: '24%' }}>Total Orders <h1>71</h1> <p style={{ fontSize: '13px' }}>22% last week</p></Box>
 
-        <Box sx={{ justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#A67B5B', color: 'white', padding: '10px', borderRadius: '7px', paddingX: '22px', width: '24%' }}>Pending <h1>13</h1> <p style={{ fontSize: '13px' }}>4% last week</p></Box>
-
-
-        <Box sx={{ justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#ECB176', color: 'white', padding: '10px', borderRadius: '7px', paddingX: '22px', width: '24%' }}>On way <h1>27</h1> <p style={{ fontSize: '13px' }}>8% last week</p></Box>
+        <Box sx={{ justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#795458', color: 'white', padding: '10px', borderRadius: '7px', paddingX: '22px', width: '24%' }}>Pending <h1>13</h1> <p style={{ fontSize: '13px' }}>4% last week</p></Box>
 
 
-        <Box sx={{ justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#FED8B1', color: 'white', padding: '10px', borderRadius: '7px', paddingX: '22px', width: '24%' }}>Completed <h1>31</h1> <p style={{ fontSize: '13px' }}>42% last week</p></Box>
+        <Box sx={{ justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#C08B5C', color: 'white', padding: '10px', borderRadius: '7px', paddingX: '22px', width: '24%' }}>On way <h1>27</h1> <p style={{ fontSize: '13px' }}>8% last week</p></Box>
+
+
+        <Box sx={{ justifyContent: 'space-around', alignItems: 'center', backgroundColor: '#FFC94A', color: 'white', padding: '10px', borderRadius: '7px', paddingX: '22px', width: '24%' }}>Completed <h1>31</h1> <p style={{ fontSize: '13px' }}>42% last week</p></Box>
 
       </Box>
 
